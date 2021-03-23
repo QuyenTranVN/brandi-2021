68396149f68538de97b5d18e763848d80d1f9f15
QuyenTranVN <tranquyenqtkdtm.bmt@gmail.com>
2021-03-23 15:28:50 +0700

    building banner

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,7 @@ const navbarSlide = () => {
 				link.style.animation = "";
 			} else {
 				link.style.animation = `navLinkFade 0.5s ease forwards 
-      ${index / 3 + 1}s`;
+      ${index / 3 + 0.5}s`;
 			}
 		});
 		// burger animation
@@ -20,3 +20,38 @@ const navbarSlide = () => {
 	});
 };
 navbarSlide();
+
+// =============//===============
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+	showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+	showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+	var i;
+	var slides = document.getElementsByClassName("banner__contain");
+	var dots = document.getElementsByClassName("dot");
+	if (n > slides.length) {
+		slideIndex = 1;
+	}
+	if (n < 1) {
+		slideIndex = slides.length;
+	}
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	for (i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slides[slideIndex - 1].style.display = "block";
+	dots[slideIndex - 1].className += " active";
+}
